58573438f26ed2a0fbfd4f52c05dd077e1b27c4e
Marc Guillen <marc@0x01.ooo>
2024-01-08 18:03:46 +0100

    Make the scrirpt more generic

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 # Dotfiles installation script
 # Launched during the devcontainer bootstrap
 
+dotfiles_dir=$(dirname "$(readlink -f "$0")")
+
 # Create symlinks
-ln -nfs $HOME/dotfiles/.bashrc $HOME/.bashrc
-for file in $HOME/dotfiles/.{aliases,functions,stack_specific,extra}; do
+ln -nfs $dotfiles_dir/.bashrc $HOME/.bashrc
+for file in $dotfiles_dir/.{aliases,functions,stack_specific,extra}; do
 	[ -r "$file" ] && [ -f "$file" ] && ln -nfs "$file" "$HOME/.$file";
 done
 unset file
